16b93aa568aa2ca6cc7cebecb3d023e29006dded
kdodeja62@gmail.com <kdodeja62@gmail.com>
2024-06-27 19:02:10 +0530

    Build Detail Stock Page

---
+++ src/app/(tabs)/[symbol].tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "@/components/Themed";
+import { Stack, useLocalSearchParams } from "expo-router";
+import top5 from "@/../assets/data/top5.json";
+import StockListItem from "@/components/StockListItem";
+
+const StockDetails = () => {
+  const { symbol } = useLocalSearchParams();
+  const stock = top5[symbol];
+  if (!stock) {
+    return <Text>Stock with Symbol{symbol} could not found</Text>;
+  }
+  return (
+    <View style={{ padding: 10 }}>
+      <Stack.Screen
+        options={{ title: stock.symbol, headerBackTitleVisible: false }}
+      />
+      <StockListItem stock={stock} />
+    </View>
+  );
+};
+
+export default StockDetails;
--- src/components/StockListItem.tsx
+++ src/components/StockListItem.tsx
@@ -1,8 +1,9 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Pressable } from "react-native";
 import { Text, View } from "./Themed";
 import Colors from "@/constants/Colors";
 import { AntDesign } from "@expo/vector-icons";
 import { MonoText } from "./StyledText";
+import { Link } from "expo-router";
 
 type Stock = {
   name: string;
@@ -16,22 +17,24 @@ type StockListItem = {
 export default function StockListItem({ stock }: StockListItem) {
   const change = Number.parseFloat(stock.percent_change);
   return (
-    <View style={styles.container}>
-      <View style={{ flex: 1, gap: 5 }}>
-        <Text style={styles.symbol}>
-          {stock.symbol}
-          <AntDesign name="staro" size={24} color="gray" />
-        </Text>
-        <Text style={{ color: "gray" }}>{stock.name}</Text>
-      </View>
-      <View style={{ alignItems: "flex-end" }}>
-        <MonoText>${Number.parseFloat(stock.close).toFixed(1)}</MonoText>
-        <MonoText style={{ color: change > 0 ? "green" : "red" }}>
-          {change > 0 ? "+" : ""}
-          {change.toFixed(1)}%
-        </MonoText>
-      </View>
-    </View>
+    <Link href={`/${stock.symbol}`} asChild>
+      <Pressable style={styles.container}>
+        <View style={{ flex: 1, gap: 5 }}>
+          <Text style={styles.symbol}>
+            {stock.symbol}
+            <AntDesign name="staro" size={24} color="gray" />
+          </Text>
+          <Text style={{ color: "gray" }}>{stock.name}</Text>
+        </View>
+        <View style={{ alignItems: "flex-end" }}>
+          <MonoText>${Number.parseFloat(stock.close).toFixed(1)}</MonoText>
+          <MonoText style={{ color: change > 0 ? "green" : "red" }}>
+            {change > 0 ? "+" : ""}
+            {change.toFixed(1)}%
+          </MonoText>
+        </View>
+      </Pressable>
+    </Link>
   );
 }
 
